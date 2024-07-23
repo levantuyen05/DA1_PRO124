e9abc46e4ac2e968b34fdaa27abdd066ec143ccf
VuTrongLich <165397523+VuTrongLich@users.noreply.github.com>
2024-07-24 02:47:54 +0700

    Lịch Trọng

--- Assets/PlayerControler.cs
+++ Assets/PlayerControler.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
+    Character player;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // player = Init.player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //player.Update();
     }
 }

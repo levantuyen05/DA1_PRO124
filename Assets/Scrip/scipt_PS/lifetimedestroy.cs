4d0d4d73e31790e8fd48feccacde57c6914418f1
levantuyen05 <167695196+levantuyen05@users.noreply.github.com>
2024-07-26 04:15:48 +0700

    update new

--- Assets/Scrip/scipt_PS/lifetimedestroy.cs
+++ Assets/Scrip/scipt_PS/lifetimedestroy.cs
@@ -1,35 +1,17 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class lifetimedestroy : MonoBehaviour
-//{
-//<<<<<<< HEAD:Assets/Scrip/scipt_PS/lifetimedestroy.cs
-//    public float time;
-//    void Start()
-//    {
-//        Destroy(this.gameObject,time);
-//=======
-//    public float moveSpeed = 10f;
-//    public  Animator animator; 
-//    private Rigidbody2D rb;
-//    public Vector3 moveInput;
-//    private void Start()
-//    {
-//        animator = GetComponent<Animator>();
-//>>>>>>> Lịch:Assets/Scrip/PlayerController.cs
-//    }
-//    private void Update()
-//    {
-//        moveInput.x = Input.GetAxis("Horizontal");
-//        moveInput.y = Input.GetAxis("Vertical");
-//        transform.position += moveInput * moveSpeed * Time.deltaTime;
-//        animator.SetFloat("Speed", moveInput.sqrMagnitude);
-//        if (moveInput.x != 0)
-//        {
-//            float rotationAngle = (moveInput.x > 0) ? 0f : 180f;
-//            transform.rotation = Quaternion.Euler(0f, rotationAngle, 0f);
-//        }
-//    }
-//}
+public class lifetimedestroy : MonoBehaviour
+{
+
+    public float time;
+    void Start()
+    {
+        Destroy(this.gameObject, time);
+
+    }
+   
+   
+}
     

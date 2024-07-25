45b6bd5abe85ff3e641ff4a5f3d1b7eb2edc0c4c
baoquoc2905 <quocvdbph48657@gmail.com>
2024-07-26 00:56:57 +0700

    upp

---
+++ Assets/Scrip/Bullet.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    public int minDamage = 0;
+    public int maxDamage = 0;
+    [SerializeField] float bulletSpeed = 10f;
+    public float lifeTime;
+    Rigidbody2D myRigidbody;
+    private Vector3 targetPosition;
+    float xSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        xSpeed = bulletSpeed;
+    }
+    public void SetTargetPosition(Vector3 position)
+    {
+        targetPosition = position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(xSpeed, 0f);
+        Destroy(gameObject, lifeTime);
+        Vector2 direction = (targetPosition - transform.position).normalized;
+
+        // Xoay viên đạn theo hướng mới
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        // Di chuyển viên đạn theo hướng mới
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = direction * bulletSpeed;
+
+        Destroy(gameObject, lifeTime);
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        // Chuẩn hóa vector hướng để có độ dài bằng 1
+        direction.Normalize();
+
+        // Tính toán góc quay từ vector hướng
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+        // Di chuyển viên đạn theo hướng chuột
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = direction * bulletSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            int damage = Random.Range(minDamage, maxDamage);
+            //collision.GetComponent<Health>().TakeDam(damage);
+            //collision.GetComponent<EnemyController>().TakeDamEffect(damage);
+            Destroy(gameObject);
+        }
+    }
+}
+
+
--- Assets/Scrip/PlayerController.cs
+++ Assets/Scrip/PlayerController.cs
@@ -1,18 +1,130 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer characterSR;
+    Animator animator;
+
+    public float dashBoost = 2f;
+    private float dashTime;
+    public float DashTime;
+    private bool once;
+    [SerializeField] GameObject Bullet;
+    [SerializeField] Transform Gun;
+    [SerializeField] float atkSpeed, countDown = 0;
+    public Vector3 moveInput;
+
+    public GameObject damPopUp;
+    //public LosePanel losePanel;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnFire();
+        /// Part 2
+        // Movement
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        transform.position += moveSpeed * Time.deltaTime * moveInput;
+        //
+
+        if (moveInput.x != 0)
+        {
+            // Tính toán góc quay dựa trên hướng di chuyển
+            float targetAngle = moveInput.x > 0 ? 0f : 180f;
+
+            // Xoay nhân vật tức thời
+            transform.eulerAngles = new Vector3(0f, targetAngle, 0f);
+        }
+
+        // Kích hoạt Animator "Run" khi đang di chuyển ngang
+        animator.SetBool("Speed", moveInput.x != 0);
+
+        if (Input.GetKey(KeyCode.E) && dashTime <= 0)
+        {
+            animator.SetBool("Roll", true);
+            moveSpeed += dashBoost;
+            dashTime = DashTime;
+            once = true;
+        }
+
+        if (dashTime <= 0 && once)
+        {
+            animator.SetBool("Roll", false);
+            moveSpeed -= dashBoost;
+            once = false;
+        }
+        else
+        {
+            dashTime -= Time.deltaTime;
+        }
+        if (Input.GetMouseButtonDown(0)) // Bắn đạn khi nhấn chuột trái
+        {
+            Shoot();
+        }
     }
+
+    //public void TakeDamageEffect(int damage)
+    //{
+    //    if (damPopUp != null)
+    //    {
+    //        GameObject instance = Instantiate(damPopUp, transform.position
+    //                + new Vector3(UnityEngine.Random.Range(-0.3f, 0.3f), 0.5f, 0), Quaternion.identity);
+    //        instance.GetComponentInChildren<TextMeshProUGUI>().text = damage.ToString();
+    //        Animator animator = instance.GetComponentInChildren<Animator>();
+    //        animator.Play("red");
+    //    }
+    //    if (GetComponent<Health>().isDead)
+    //    {
+    //        losePanel.Show();
+    //    }
+    //}
+    void OnFire()
+    {
+        countDown -= Time.deltaTime;
+        if (countDown > 0)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Bullet, Gun.position, transform.rotation);
+            countDown = atkSpeed;
+            Invoke("ResetAttackAnimation", 0.5f);
+
+        }
+    }
+    void Shoot()
+    {
+        // Tính toán hướng từ nhân vật đến vị trí chuột
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+
+        // Tạo viên đạn và xoay nó theo hướng chuột
+        GameObject bullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+        Bullet bulletScript = bullet.GetComponent<Bullet>();
+
+        if (bulletScript != null)
+        {
+            bulletScript.SetTargetPosition(mousePosition); // Truyền vị trí mục tiêu cho viên đạn
+        }
+    }
+
 }

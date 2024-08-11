5b2a426d8461d519f253b934a50937dcf3ccc511
levantuyen05 <167695196+levantuyen05@users.noreply.github.com>
2024-08-11 11:16:51 +0700

    updatenew

---
+++ Assets/Scrip/scipt_PS/lifetimedestroy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lifetimedestroy : MonoBehaviour
+{
+    public float time;
+    void Start()
+    {
+        Destroy(this.gameObject,time);
+    }
+
+   
+}
